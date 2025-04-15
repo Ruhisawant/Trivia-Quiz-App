44f63aeeec94f643cbf2a01175588202abc699dc
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-04-15 14:29:42 -0400

    Added demo code

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,19 @@
 import 'package:flutter/material.dart';
+import 'quiz_screen.dart';
 
 void main() {
-  runApp(const TriviaApp());
+  runApp(MyApp());
 }
 
-class TriviaApp extends StatelessWidget {
-  const TriviaApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Trivia Quiz App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Trivia Quiz'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-          ],
-        ),
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: QuizScreen(),
     );
   }
 }
